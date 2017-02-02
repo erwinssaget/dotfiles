92e8f64cfe663474edf6961885baa7e01f0bacbe
Erwins Saget <erwinsaget@gmail.com>
2017-02-02 03:33:55 -0500

    tmux changes and install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Set of things I do on new installs
+sudo apt update
+
+sudo apt upgrade
+
+# Install git and vim
+sudo apt install -y git vim zsh tmux wget curl
+
+# Clone repo
+# Set up oh my zsh, git flow avh
+# make symlinks
+# install php composer
+# spotify
+
